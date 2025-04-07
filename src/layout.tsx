030596a54133eee286e0d39151b597dfa22fc2b0
An Nguyen <anndb1903@gmail.com>
2025-04-07 12:47:25 +0700

    Xử lý loading

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,16 +4,18 @@ import AppFooter from "components/layout/app.footer"
 import { useEffect } from "react"
 import { fetchAccountAPI } from "services/api"
 import { useCurrentApp } from "components/context/app.context"
+import PacmanLoader from "react-spinners/PacmanLoader"
 
 
 const Layout = () => {
-  const {setUser, isAppLoading, setIsAppLoading} = useCurrentApp()
+  const {setUser, isAppLoading, setIsAppLoading, setIsAuthenticated} = useCurrentApp()
 
   useEffect(() => {
     const fetchAccount = async () => {
       const res = await fetchAccountAPI();
       if (res.data) {
         setUser(res.data.user)
+        setIsAuthenticated(true)
       }
       setIsAppLoading(false)
     }
@@ -21,11 +23,20 @@ const Layout = () => {
     fetchAccount();
   }, []) // [] là gọi 1 lần. useEffect là render giao diện rr mới chạy vào
   return (
-    <div>
-      <AppHeader />
-      <Outlet />
-      <AppFooter />
-    </div>
+    <>
+      {isAppLoading === false ? (
+        <div>
+          <AppHeader />
+          <Outlet />
+          <AppFooter />
+        </div>
+      ) : (
+        // src nằm giữa màn hình
+        <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+          <PacmanLoader size={30} color="#36d6b4"/>
+        </div>
+      )}
+    </>
   )
 }
 
