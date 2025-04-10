292ddb966d886c337f7ca41cf5574037d168592e
An Nguyen <anndb1903@gmail.com>
2025-04-10 09:53:32 +0700

    mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "@/layout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import BookPage from "pages/client/book";
 import AboutPage from "pages/client/about";
 import LoginPage from "pages/client/auth/login";
 import RegisterPage from "pages/client/auth/register";
-import "styles/glocal.scss";
+import "styles/global.scss";
 import HomePage from "pages/client/home";
 import { App, ConfigProvider } from "antd";
 import { AppProvider } from "components/context/app.context";
-import ProtectedRoute from "components/auth";
-import ManageOrderPage from "pages/admin/manage.order";
-import ManageBookPage from "pages/admin/manage.book";
+import ProtectedRoute from "@/components/auth";
 import DashBoardPage from "pages/admin/dashboard";
+import ManageBookPage from "pages/admin/manage.book";
+import ManageOrderPage from "pages/admin/manage.order";
 import ManageUserPage from "pages/admin/manage.user";
-import enUS from "antd/locale/en_US";
 import LayoutAdmin from "components/layout/layout.admin";
-import OrderPage from "./pages/client/order";
-import HistoryPage from "./pages/client/history";
+import OrderPage from "pages/client/order";
+import HistoryPage from "pages/client/history";
+import enUS from "antd/locale/en_US";
 // import viVN from "antd/locale/vi_VN";
+import ReturnURLPage from "components/client/order/return.url";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const router = createBrowserRouter([
   {
@@ -53,6 +55,14 @@ const router = createBrowserRouter([
             <HistoryPage />
           </ProtectedRoute>
         )
+      },
+      {
+        path: "/vnpay/return-url",
+        element: (
+          <ProtectedRoute>
+            <ReturnURLPage />
+          </ProtectedRoute>
+        )
       }
     ]
   },
@@ -84,11 +94,20 @@ const router = createBrowserRouter([
           </ProtectedRoute>
         )
       },
+
       {
         path: "user",
         element: (
           <ProtectedRoute>
             <ManageUserPage />
+          </ProtectedRoute>
+        )
+      },
+      {
+        path: "/admin",
+        element: (
+          <ProtectedRoute>
+            <div>admin page</div>
           </ProtectedRoute>
         )
       }
@@ -108,9 +127,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App>
       <AppProvider>
-        <ConfigProvider locale={enUS}>
-          <RouterProvider router={router} />
-        </ConfigProvider>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <ConfigProvider locale={enUS}>
+            <RouterProvider router={router} />
+          </ConfigProvider>
+        </GoogleOAuthProvider>
       </AppProvider>
     </App>
   </StrictMode>
